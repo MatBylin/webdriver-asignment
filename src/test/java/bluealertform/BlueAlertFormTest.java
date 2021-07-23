3775880e2e7d206f087776b4627759c940534924
MatBylin <matbylin@gmail.com>
2021-07-23 22:21:58 +0200

    Add assertion and update BlueAlertFormTest

--- src/test/java/bluealertform/BlueAlertFormTest.java
+++ src/test/java/bluealertform/BlueAlertFormTest.java
@@ -2,13 +2,16 @@ package bluealertform;
 
 import base.BaseTest;
 import org.junit.jupiter.api.Test;
+import pages.BlueAlertFormAgreementPage;
 
 import static constants.TestConstants.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BlueAlertFormTest extends BaseTest {
 
     @Test
-    public void test() {
+    public void fillBlueAlertFormPageValidateRedirectionToAgreementPage() {
         blueAlertFormPage
                 .load()
                 .fillFirstName(VALID_FIRST_NAME)
@@ -17,7 +20,9 @@ public class BlueAlertFormTest extends BaseTest {
                 .fillPhoneNumber(VALID_PHONE_NUMBER)
                 .fillPeselNumber(VALID_PESEL_NUMBER)
                 .fillIdNumber(VALID_ID_NUMBER)
-                .fillDate(VALID_DATE)
-                .clickConfirmButton();
+                .fillDate(VALID_DATE);
+
+        BlueAlertFormAgreementPage agreementPage = blueAlertFormPage.clickConfirmButton();
+        assertThat(agreementPage.isPageTitlePresent(), is(true));
     }
 }
